fa0a0f50e9ec7cab0248d377f19a8980b00c786d
Luis Carlos <luisczdidi@gmail.com>
2021-12-24 10:15:40 -0300

    First Commit
    Project started using Spring Initializr

---
+++ src/main/java/com/dio/meetingroombackend/MeetingroombackendApplication.java
@@ -0,0 +1,13 @@
+package com.dio.meetingroombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroombackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroombackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/meetingroombackend/MeetingroombackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.meetingroombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroombackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
